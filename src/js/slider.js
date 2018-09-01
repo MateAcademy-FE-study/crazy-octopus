f6b3685f48a6d65272944e775cedea6edff2f775
Oksana <oksanashukh@gmail.com>
2018-09-02 00:42:26 +0300

    fixed slideshow dots

--- src/js/slider.js
+++ src/js/slider.js
@@ -13,14 +13,21 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block"; 
 
 
-   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-        if (i === slideIndex) {
-  dots[i].className = "dot active";
-} else {
-  dots[i].className = "dot";
-}
-  }
+//    for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//         if (i === slideIndex) {
+//   dots[i].className = "dot active";
+// } else {
+//   dots[i].className = "dot";
+// }
+//   }
 
+var dotActive = document.querySelector('.dot.active');
+if (dotActive == null) { // if no dot active...
+    document.querySelector('.dot').classList.add("active");
+} else { // remove current active and set it to next ele
+    dotActive.classList.remove("active");
+    (dotActive.nextElementSibling || document.querySelector('.dot')).classList.add("active");
+}
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
